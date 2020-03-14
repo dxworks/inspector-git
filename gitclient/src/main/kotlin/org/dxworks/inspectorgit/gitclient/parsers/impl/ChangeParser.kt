dec7c9105cb9d3d716e03bd23d1b3a1bfc74ef81
dnagy <Tengo.,123>
2020-03-14 17:51:04 +0200

    Fixed change parsers file name extraction minding binary files as well

--- gitclient/src/main/kotlin/org/dxworks/inspectorgit/gitclient/parsers/impl/ChangeParser.kt
+++ gitclient/src/main/kotlin/org/dxworks/inspectorgit/gitclient/parsers/impl/ChangeParser.kt
@@ -55,10 +55,30 @@ class ChangeParser(private val parentCommitId: String) : GitParser<ChangeDTO> {
     }
 
     private fun extractFileNames(lines: List<String>, type: ChangeType): Pair<String, String> {
-        val oldFilePrefix = "--- a/"
-        val newFilePrefix = "+++ b/"
-        val oldFileName = if (type == ChangeType.ADD) devNull else lines.find { it.startsWith(oldFilePrefix) }!!.removePrefix(oldFilePrefix)
-        val newFileName = if (type == ChangeType.DELETE) devNull else lines.find { it.startsWith(newFilePrefix) }!!.removePrefix(newFilePrefix)
+        val oldFilePrefix = if(type == ChangeType.RENAME) "rename from " else "--- a/"
+        val newFilePrefix = if(type == ChangeType.RENAME) "rename to " else "+++ b/"
+
+        val oldFileName = if (type == ChangeType.ADD) devNull else {
+            extractFileName(lines, oldFilePrefix)
+        }
+        val newFileName = if (type == ChangeType.DELETE) devNull else {
+            extractFileName(lines, newFilePrefix)
+        }
         return Pair(oldFileName, newFileName)
+    }
+
+    private fun extractFileName(lines: List<String>, fileNamePrefix: String): String {
+        return try {
+            lines.find { it.startsWith(fileNamePrefix) }!!.removePrefix(fileNamePrefix)
+        } catch (e: NullPointerException) {
+            extractFileName(lines[0])
+        }
+    }
+
+    private fun extractFileName(diffLine: String): String {
+        val namesStartIndex = diffLine.indexOf(" a/") + 3
+        val names = diffLine.substring(namesStartIndex)
+        val namesParts = names.split(" b/")
+        return namesParts.take(namesParts.size / 2).joinToString(" b/")
     }
 }
