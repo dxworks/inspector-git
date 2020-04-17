6e7c6072f0df2e5e11d892360e0e79baaf68431f
dnagy <nagydarius97@gmail.com>
2020-04-17 12:56:02 +0300

    Added deleted lines and added lines properties to hunk and change

--- core/src/main/kotlin/org/dxworks/inspectorgit/model/Hunk.kt
+++ core/src/main/kotlin/org/dxworks/inspectorgit/model/Hunk.kt
@@ -1,3 +1,14 @@
 package org.dxworks.inspectorgit.model
 
-class Hunk(val lineChanges: List<LineChange>)
+import org.dxworks.inspectorgit.gitclient.enums.LineOperation
+
+class Hunk(val lineChanges: List<LineChange>) {
+    val deletedLines: List<LineChange>
+    val addedLines: List<LineChange>
+
+    init {
+        val (deletedLines, addedLines) = lineChanges.partition { it.operation == LineOperation.DELETE }
+        this.deletedLines = deletedLines
+        this.addedLines = addedLines
+    }
+}
