652ef0239a079be7aadcbefde2e70b5874816c14
Darius Nagy <nagydarius97@gmail.com>
2020-07-01 18:51:55 +0300

    Fixed OsUtils

--- utils/src/main/kotlin/org/dxworks/inspectorgit/utils/OsUtils.kt
+++ utils/src/main/kotlin/org/dxworks/inspectorgit/utils/OsUtils.kt
@@ -3,9 +3,11 @@ package org.dxworks.inspectorgit.utils
 class OsUtils {
     companion object {
         private val osName by lazy { System.getProperty("os.name") }
-        val isWindows by lazy { osName.toLowerCase().contains("win") }
-        val isUnix by lazy { !isWindows }
-        val commandInterpreterPrefix by lazy { if (isUnix) "bash" else "cmd.exe" }
+        val isWindows by lazy { osName.contains("win", ignoreCase = true) }
+        val isLinux by lazy { osName.contains("nux", ignoreCase = true) || osName.contains("nix", ignoreCase = true) }
+        val isMac by lazy { osName.contains("mac", ignoreCase = true) }
+        val isUnix by lazy { isLinux || isMac }
+        val commandInterpreterName by lazy { if (isUnix) "bash" else "cmd.exe" }
         val interpreterArg by lazy { if (isUnix) "-c" else "/C" }
     }
 }
