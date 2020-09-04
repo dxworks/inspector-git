234eb188f37428fb139270f7c73fef7bf3fef5f4
Darius Nagy <nagydarius97@gmail.com>
2020-09-04 23:40:10 +0300

    Removed AnnotatedLine

--- core/src/main/kotlin/org/dxworks/inspectorgit/model/git/AnnotatedLine.kt
+++
@@ -1,7 +0,0 @@
-package org.dxworks.inspectorgit.model.git
-
-data class AnnotatedLine(var number: Int, var content: AnnotatedContent) {
-    override fun toString(): String {
-        return "${content.commit.id} (${content.commit.author.id} ${content.commit.committerDate} $number) ${content.content}"
-    }
-}
