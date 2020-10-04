6df6b54928c09fb60cb827e353cd46c391cafd91
Darius Nagy <nagydarius97@gmail.com>
2020-10-04 21:14:43 +0300

    Added messageClassifier to ScriptUtils.kt

--- services/src/main/kotlin/org/dxworks/inspectorgit/services/ScriptUtils.kt
+++ services/src/main/kotlin/org/dxworks/inspectorgit/services/ScriptUtils.kt
@@ -6,6 +6,8 @@ fun exportCsv(list: List<Map<String, Any>>, filenameWithoutExtension: String) {}
 
 val system: LoadedSystem = LoadedSystem()
 
+val messageClassifier: MessageClassifierService = MessageClassifierService()
+
 fun mergeAccounts(name: String, vararg idsToMerge: String) {}
 
 fun mergeDevelopers(name: String, vararg idsToMerge: String) {}
