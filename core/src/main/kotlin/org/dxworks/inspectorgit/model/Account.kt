9aed80720dbb21e8ad9fe8cbed8655dcb0a3ddbf
Darius Nagy <nagydarius97@gmail.com>
2020-11-18 00:05:02 +0200

    Added equals and hashcode to account

--- core/src/main/kotlin/org/dxworks/inspectorgit/model/Account.kt
+++ core/src/main/kotlin/org/dxworks/inspectorgit/model/Account.kt
@@ -6,4 +6,19 @@ abstract class Account(
         var developer: Developer? = null
 ) {
     abstract val id: String
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Account
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
 }
