69188d983cecf0308b540626572a01963af1e46a
dnagy <dariusnicolae.nagy@endava.com>
2021-10-15 11:34:35 +0300

    Fixed chronos merge changes

--- chr-helper/src/main/kotlin/org/dxworks/inspectorgit/chr/Main.kt
+++ chr-helper/src/main/kotlin/org/dxworks/inspectorgit/chr/Main.kt
@@ -10,35 +10,38 @@ import java.io.FileFilter
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
-    val folder = Paths.get(args.firstOrNull() ?: ".").toFile()
-    folder.isDirectory
+    val arguments = args.toMutableList()
+    val prefix = arguments.remove("--prefix")
+
+    val folder = Paths.get(arguments.firstOrNull() ?: ".").toFile()
 
     val files = folder.listFiles(FileFilter { it.extension == "iglog" }) ?: return
-    val doPrefix = files.size > 1
+    val doPrefix = files.size > 1 || prefix
 
     val changeMeta = files.map {
         GitProjectTransformer(
-            IGLogReader().read(it.inputStream()),
-            it.nameWithoutExtension
+                IGLogReader().read(it.inputStream()),
+                it.nameWithoutExtension
         ).transform()
     }.flatMap { project ->
         project.commitRegistry.all.filterNot { it.isMergeCommit }.flatMap { it.changes }.map { it to project.name }
     }.associate {
         val change = it.first
         getId(it, doPrefix) to mapOf(
-            "fileSize" to change.annotatedLines.size,
-            "isAlive" to !(it == change.file.changes.last() && change.type == ChangeType.DELETE)
+                "fileSize" to change.annotatedLines.size,
+                "isAlive" to !(it == change.file.changes.last() && change.type == ChangeType.DELETE)
         )
     }
-
-    val file = folder.resolve("chr-help.json")
-    file.createNewFile()
-    jacksonObjectMapper().writeValue(file, changeMeta)
+    if (changeMeta.isNotEmpty()) {
+        val file = folder.resolve("chr-help.json")
+        file.createNewFile()
+        jacksonObjectMapper().writeValue(file, changeMeta)
+    }
 }
 
 private fun getId(
-    changeAndProjectName: Pair<Change, String>,
-    doPrefix: Boolean
+        changeAndProjectName: Pair<Change, String>,
+        doPrefix: Boolean
 ) = (getFileName(changeAndProjectName.first) + changeAndProjectName.first.commit.id).let {
     if (doPrefix)
         "${changeAndProjectName.second}/$it${changeAndProjectName.second}"
